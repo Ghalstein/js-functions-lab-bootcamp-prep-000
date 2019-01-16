46f828c72743618fc6f87be4dd21501a5549bd7a
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-16 05:25:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
-function happyHolidaysTo(name){
+function happyHolidaysTo("name"){
   return "Happy holidays, ${name}!"
 }
 function happyHolidayTo(holiday, name){
